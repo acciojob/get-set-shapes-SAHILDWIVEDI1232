e2e8ccc91c73b1e55746935765c9ec15379bb5f2
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-08-22 23:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//complete this code
 class Rectangle {
-	constructor(width,height){
-		this._width=width;
-		this._height=height;
+	constructor(width, height) {
+		this._width = width;
+		this._height = height;
 	}
+
 	get width() {
 		return this._width;
 	}
@@ -11,29 +11,33 @@ class Rectangle {
 	get height() {
 		return this._height;
 	}
-	getArea(){
-		return (this.width*this.height);
+
+	getArea() {
+		return this.width * this.height;
 	}
 }
 
 class Square extends Rectangle {
-	constructor(side){
-		super(side,side);
-		
-	
-	getPerimeter(){
-		return (this.width*4);
+	constructor(side) {
+		super(side, side);
+	}
+
+	getPerimeter() {
+		return this.width * 4;
 	}
 }
-const rectangle=new Rectangle(5,10);
+
+// Test output (optional, for local verification)
+const rectangle = new Rectangle(5, 10);
 console.log(rectangle.width); // Output: 5
 console.log(rectangle.height); // Output: 10
-console.log(rectangle.getArea());
+console.log(rectangle.getArea()); // Output: 50
+
 const square = new Square(7);
 console.log(square.width); // Output: 7
 console.log(square.height); // Output: 7
 console.log(square.getArea()); // Output: 49
-console.log(square.getPerimeter()); 
+console.log(square.getPerimeter()); // Output: 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
